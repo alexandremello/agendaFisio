736a98d36bf1e698f81506c24f044e89ebf247a2
solfier <alexandre.rmello@gmail.com>
2013-06-03 21:30:09 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgendaFisio::Application.config.secret_token = '5f7df9df488631e68ad9ae1274844b593d8c29d6e116a8b2a28dfeca0fc278b43877018c1dea8148877dd3b106c14e430634593b9ebf8379a0d5eb93e0aafbcf'
