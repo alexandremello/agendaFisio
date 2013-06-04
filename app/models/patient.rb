8c883149457e6c98cb65575b968ece3b65b61d9b
solfier <alexandre.rmello@gmail.com>
2013-06-04 13:45:10 -0300

    added devise

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,6 @@ class Patient < ActiveRecord::Base
   attr_accessible :address, :birth, :name
 
   has_many :exams
+
+  validates_presence_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AgendaFisio::Application.routes.draw do
+  devise_for :users
+
   resources :exams, only: :index
 
   resources :patients do
