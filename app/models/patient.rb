59b68cd5618bbcc1fb3827ff3007995856a447a5
ardmello <alexandre.rmello@gmail.com>
2013-06-14 12:20:03 -0300

    set up relationships with user.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,6 +3,7 @@ class Patient < ActiveRecord::Base
 
   has_many :exams
   has_many :appointments
+  belongs_to :user
 
 	validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130614150202) do
+ActiveRecord::Schema.define(:version => 20130614151709) do
 
   create_table "appointments", :force => true do |t|
     t.datetime "start"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130614150202) do
     t.string   "address"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
